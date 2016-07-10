f5f043cc54349476a61e692a476a2ca6fd350cf6
Matteo Taroli <taroli.matteo@gmail.com>
2016-07-10 20:06:36 +0200

    Change license

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ This app is mainly done to experiment with new libraries and stuff. It uses :
 - [Retrofit](https://square.github.io/retrofit/) to handle HTTP requests
 - [Butter Knife](https://jakewharton.github.io/butterknife/) to bind views and methods
 - [Picasso](https://square.github.io/picasso/) to download and cache pictures
+
+The app is far from done, but I learnt what I wanted to from this project.
+I most likely won't work on it anymore, so feel free to use it in any way you want :)
--- app/src/main/java/be/matteotaroli/watch/utils/Constants.java
+++ app/src/main/java/be/matteotaroli/watch/utils/Constants.java
@@ -1,22 +1,3 @@
-/*
-* Watch allows to search and save movies to watch later or just to keep it handy.
-*
-* Copyright (C) 2016 Matteo Taroli
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
-
 package be.matteotaroli.watch.utils;
 
 public class Constants {
